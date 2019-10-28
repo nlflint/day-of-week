a05578e5758ec51509591bd13b507516f35fd464
Nathan Flint <nlflint@gmail.com>
2019-10-28 11:12:59 -0700

    Handling every 4 years is leap year

--- day-of-week.rkt
+++ day-of-week.rkt
@@ -2,7 +2,7 @@
 (require rackunit)
 
 
-; Sunday October 1, 1752 is anchor
+; Month calculations
 
 (define (day-of-year-by-month month acc)
   (match month
@@ -23,16 +23,45 @@
 (check-equal? (day-of-year-by-month 2 0) 31)
 (check-equal? (day-of-year-by-month 12 0) 334)
 
+
+; Leap year calculations
+(define leap-years-on-and-before-1752
+  (/ 1752 4))
+
+(define (is-leap-year year)
+  (and
+   (= 0 (remainder year 4))
+   (> year 1752)))
+
+(define (leap-day-count year month)
+  (+ (floor (/ year 4))
+     (- 0 leap-years-on-and-before-1752)
+     (- (if (and (is-leap-year year) (< month 3)) 1 0 ))))
+
+(check-equal? (leap-day-count 1752 1) 0)
+(check-equal? (leap-day-count 1752 5) 0)
+(check-equal? (leap-day-count 1753 1) 0)
+(check-equal? (leap-day-count 1754 1) 0)
+(check-equal? (leap-day-count 1755 1) 0)
+(check-equal? (leap-day-count 1756 1) 0)
+(check-equal? (leap-day-count 1756 2) 0)
+(check-equal? (leap-day-count 1756 3) 1)
+
+; Day calculations
 (define (day-of-week year month day)
   (let ([days-since-anchor (+
                             (- day 1)
                             (day-of-year-by-month month 0)
-                            (* 365 (- year 1752)))])
+                            (* 365 (- year 1752))
+                            (leap-day-count year month))])
     (+ 1 (remainder days-since-anchor 7))))
 
 
 
 ;handle by day
+(check-equal? (day-of-week 1752 9 14) 5)
+(check-equal? (day-of-week 1752 9 24) 1)
+(check-equal? (day-of-week 1752 9 30) 7)
 (check-equal? (day-of-week 1752 10 1) 1)
 (check-equal? (day-of-week 1752 10 7) 7)
 
@@ -49,3 +78,13 @@
 (check-equal? (day-of-week 1753 7 28) 7)
 (check-equal? (day-of-week 1754 9 13) 6)
 (check-equal? (day-of-week 1755 10 10) 6)
+(check-equal? (day-of-week 1756 2 28) 7)
+(check-equal? (day-of-week 1756 2 29) 1)
+
+;handle a leap year
+(check-equal? (day-of-week 1756 3 1) 2)
+(check-equal? (day-of-week 1775 1 3) 3)
+(check-equal? (day-of-week 1798 12 12) 4)
+(check-equal? (day-of-week 1799 12 31) 3)
+(check-equal? (day-of-week 1800 1 1) 4)
+(check-equal? (day-of-week 1800 2 28) 6)
