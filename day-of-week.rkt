0776c2f4d1b3e4396abbbc876448899c479a000d
Nathan Flint <nlflint@gmail.com>
2019-10-28 09:52:07 -0700

    Handling months now

--- day-of-week.rkt
+++ day-of-week.rkt
@@ -2,12 +2,45 @@
 (require rackunit)
 
 
-(define (day-of-week year month day)
-  (+ 1 (remainder (- day 1) 7)))
-
 ; Sunday October 1, 1752 is anchor
 
+(define (day-of-year-by-month month acc)
+  (match month
+    [1 acc]
+    [2 (day-of-year-by-month (- month 1) (+ acc 31))]
+    [3 (day-of-year-by-month (- month 1) (+ acc 28))]
+    [4 (day-of-year-by-month (- month 1) (+ acc 31))]
+    [5 (day-of-year-by-month (- month 1) (+ acc 30))]
+    [6 (day-of-year-by-month (- month 1) (+ acc 31))]
+    [7 (day-of-year-by-month (- month 1) (+ acc 30))]
+    [8 (day-of-year-by-month (- month 1) (+ acc 31))]
+    [9 (day-of-year-by-month (- month 1) (+ acc 31))]
+    [10 (day-of-year-by-month (- month 1) (+ acc 30))]
+    [11 (day-of-year-by-month (- month 1) (+ acc 31))]
+    [12 (day-of-year-by-month (- month 1) (+ acc 30))]))
+
+(check-equal? (day-of-year-by-month 1 0) 0)
+(check-equal? (day-of-year-by-month 2 0) 31)
+(check-equal? (day-of-year-by-month 12 0) 334)
+
+(define (day-of-week year month day)
+  (let ([days-since-anchor (+
+                            (- day 1)
+                            (day-of-year-by-month month 0))])
+    (+ 1 (remainder days-since-anchor 7))))
+
+
+
+;handle by day
 (check-equal? (day-of-week 1752 10 1) 1)
 (check-equal? (day-of-week 1752 10 7) 7)
+
+;handle after first week
 (check-equal? (day-of-week 1752 10 8) 1)
 (check-equal? (day-of-week 1752 10 31) 3)
+
+;handle after month
+(check-equal? (day-of-week 1752 11 1) 4)
+(check-equal? (day-of-week 1752 12 31) 1)
+
+
