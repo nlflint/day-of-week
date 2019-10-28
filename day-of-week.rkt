4b434fcc93a4203c5aafdb488b6a62b32187148b
Nathan Flint <nlflint@gmail.com>
2019-10-28 09:28:15 -0700

    Handling beyond firs week

--- day-of-week.rkt
+++ day-of-week.rkt
@@ -2,12 +2,12 @@
 (require rackunit)
 
 
-
-
 (define (day-of-week year month day)
-  1)
+  (+ 1 (remainder (- day 1) 7)))
 
-; Sunday October 1, 1752
-
+; Sunday October 1, 1752 is anchor
 
 (check-equal? (day-of-week 1752 10 1) 1)
+(check-equal? (day-of-week 1752 10 7) 7)
+(check-equal? (day-of-week 1752 10 8) 1)
+(check-equal? (day-of-week 1752 10 31) 3)
